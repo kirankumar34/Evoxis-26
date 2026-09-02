5145530af916c959b1f042ba134c7cf580c17814
Gowtham-Babu-2706 <gowthambabu27062006@gmail.com>
2026-09-02 11:47:35 +0530

    after removeing the error

--- src/components/departments/DepartmentsSection.tsx
+++ src/components/departments/DepartmentsSection.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 import { DEPARTMENTS } from '@/data/departments';
-import { Anchor } from 'lucide-react';
 
 export const DepartmentsSection: React.FC = () => {
   return (
